c939943ef7183e937e6c7069951b088c8aca8797
shubham21197 <shubham21197@gmail.com>
2023-09-22 23:22:49 +1000

    mobile device optimisations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { ScrollService } from '../services/scroll.service';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
   @Input() onTop = true;
+  mobile = false;
 
-  constructor(private scrollService: ScrollService) { }
-  
+  constructor(
+    private scrollService: ScrollService,
+    private themeService: ThemeService
+  ) {
+    this.mobile = window.innerWidth < 1220;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.mobile = window.innerWidth < 1220;
+  }
+
+  get lightMode() {
+    return this.themeService.lightMode;
+  }
+
   scrollTo(page: string) {
     this.scrollService.scrollToPage.emit(page);
   }
 
+  downloadResume() {}
 }
